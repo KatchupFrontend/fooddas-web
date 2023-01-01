46a2dfe3c61a3785946e4719fe8de71508dddc47
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-01-01 22:49:41 +0000

    firebase submission

--- component/Tabs.jsx
+++ component/Tabs.jsx
@@ -1,31 +1,33 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 const Tabs = () => {
   return (
     <div>
-      <div className="flex m-10 md:justify-between flex-col">
+      <div className=" m-10  flex flex-wrap justify-between ">
         <div className="py-3">
-          <h2 className="text-2xl font-bold">Categories</h2>
+          <h2 className="text-2xl font-bold pr-4">Categories</h2>
         </div>
-        <div className="">
+        <div className="flex items-center">
           <input
             type="text"
-            className="bg-gray-100 rounded-lg  p-2 focus:ring-2 focus:border-gray-300 focus:ring-gray-300 outline-none"
-            placeholder="Search what you love"
+            className="bg-gray-100 rounded-lg px-6  p-2 focus:ring-none focus:border-none focus:ring-gray-300 outline-none relative"
+            placeholder="Search what you love "
           />
+          <FaSearch className="absolute mx-1" color="gray"/>
         </div>
       </div>
       <div className="mx-3">
-        <button className="bg-red-100  px-5 py-2 m-2 rounded-xl ">All</button>
-        <button className="bg-red-100 px-5 py-2 m-2 rounded-xl">
+        <button className="bg-red-100   px-5 py-2 m-2 rounded-2xl  hover:bg-red-600 hover:text-white">All</button>
+        <button className="bg-red-100 px-5 py-2 m-2 rounded-xl hover:bg-red-600  hover:text-white">
           Breakfast
         </button>
-        <button className="bg-red-100 px-5 py-2 m-2 rounded-xl">Launch</button>
-        <button className="bg-red-100 px-5 py-2 m-2 rounded-xl">Dinner</button>
-        <button className="bg-red-100 px-5 py-2 m-2 rounded-xl">Sauce </button>
-        <button className="bg-red-100 px-5 py-2 m-2 rounded-xl">Stew</button>
-        <button className="bg-red-100 px-5 py-2 m-2 rounded-xl">Soup</button>
-        <button className="bg-red-100 px-5 py-2 m-2 rounded-xl">Pizza</button>
+        <button className="bg-red-100 px-5 py-2 m-2 rounded-xl hover:bg-red-600 hover:text-white">Launch</button>
+        <button className="bg-red-100 px-5 py-2 m-2 rounded-xl hover:bg-red-600 hover:text-white">Dinner</button>
+        <button className="bg-red-100 px-5 py-2 m-2 rounded-xl hover:bg-red-600 hover:text-white">Sauce </button>
+        <button className="bg-red-100 px-5 py-2 m-2 rounded-xl hover:bg-red-600 hover:text-white">Stew</button>
+        <button className="bg-red-100 px-5 py-2 m-2 rounded-xl hover:bg-red-600 hover:text-white">Soup</button>
+        <button className="bg-red-100 px-5 py-2 m-2 rounded-xl hover:bg-red-600 hover:text-white">Pizza</button>
       </div>
     </div>
   );
--- pages/Checkout.jsx
+++ pages/Checkout.jsx
@@ -3,11 +3,14 @@ import Layout from '../component/Layout'
 import { useContext } from 'react';
 import { Store } from '../context/Store';
 import { useState } from 'react';
-// import PaystackPop from '@paystack/inline-js';
+
 
 import Link from 'next/link';
+import { UserAuth } from '../context/AuthContext';
+import { addDoc , collection} from 'firebase/firestore';
 
 const Checkout = () => {
+  const {user} = UserAuth();
    const [email, setEmail] = useState("");
    const [name, setName] = useState("");
    const [phone, setPhone] = useState("");
@@ -22,28 +25,31 @@ const Checkout = () => {
       dispatch({ type: "CART_REMOVE_ITEM", payload: food });
     };
 
+    const checkoutHandler = async (e) => {
+      e.preventDefault();
+      if(!name || !email || !phone){
+        alert('Please fill in all fields')
+      }
+      else{
+        const {uid, email} = user;
+        await addDoc(collection(db, "orderedItems"), {
+          uid,
+          email,
+          name,
+          phone,
+          cartItems,
+          Total,
+        });
+        dispatch({ type: "CART_CLEAR" }); 
+      }
+    };
 
-    //  const paywithPaystack = (e) => {
-    //    e.preventDefault();
 
-    //    const paystack = new PaystackPop();
-    //    paystack.newTransaction({
-    //      key: "pk_live_ceb5e28498396c908f35a84006294fb2c4360b36",
-    //      email: email,
-    //      amount: amount * 100,
+      
+    //   props.history.push("/signin?redirect=shipping");
+    // };
 
-    //      //Successful payment
-    //      onSuccess(transaction) {
-    //        let msg = `Transaction Successful ${transaction.reference}`;
-    //        alert(msg);
-    //        form.current.reset();
-    //      },
-    //      onCancel() {
-    //        alert("Transaction Cancelled");
-    //      },
-    //    });
-    //  };
-
+    
 
 
   return (
@@ -84,11 +90,10 @@ const Checkout = () => {
               </svg>
               <p className="text-sm leading-none">Back</p>
             </button>
-            <p className="text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9 text-gray-800">
+            <p className="text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9 text-gray-800 text-center">
               Checkout
             </p>
             <p className="text-base leading-normal sm:leading-4 text-gray-600">
-              Home {">"} Electronics {">"} Headphones {">"} Cart {">"} Checkout
             </p>
           </div>
 
@@ -201,8 +206,8 @@ const Checkout = () => {
                 </div>
               </div>
             </div>
-            <form action="">
-              <div className="p-8 bg-gray-100 flex flex-col lg:w-full xl:w-3/5">
+          
+              <form className="p-8 bg-gray-100 flex flex-col lg:w-full xl:w-3/5" onSubmit={checkoutHandler}>
                 <h1 className="font-bold text-center text-2xl">
                   Customer Details
                 </h1>
@@ -244,142 +249,16 @@ const Checkout = () => {
                   onClick={""}
                 >
                   <div>
-                    <p className="text-base leading-4">Complete Order</p>
+                    <p className="text-base leading-4">Place Order</p>
                   </div>
                 </button>
-              </div>
-            </form>
+              </form>
+          
           </div>
         </div>
       </div>
     </div>
   );
-
-      //  <div>
-      //   <div className=" sm:inline-block md:flex">
-      //     <div className="md:w-2/3 ">
-      //       <div class="">
-      //         <div class="py-12">
-      //           <div class="max-w-md  rounded-lg md:max-w-xl ">
-                 
-      //           </div>
-      //         </div>
-      //       </div>
-      //     </div>
-      //     <div className="md:w-1/3 md:flex md:justify-end ">
-      //       <div class="flex   justify-center md:mx-5">
-      //         <div class=" flex justify-center items-center ">
-      //           <div class="  justify-center items-center flex max-w-full ">
-      //             <div class=" w-screen max-w-md ">
-      //               <div class="flex h-full flex-col  ">
-      //                 <div class="flex-1  py-6 px-4 sm:px-6 ">
-      //                   <div class="mt-8">
-      //                     <div class="flow-root">
-      //                       {cartItems.length === 0 ? (
-      //                         <div>
-      //                           Cart is empty.
-      //                           <Link href="/customer">Make an order</Link>
-      //                         </div>
-      //                       ) : (
-      //                         <div className="mt-8">
-      //                           <div className="flow-root">
-      //                             <ul className="-my-6 divide-y divide-gray-200">
-      //                               {cartItems.map((food) => (
-      //                                 <li className="flex py-6" key={food.id}>
-      //                                   <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-      //                                     <img
-      //                                       src={food.image}
-      //                                       alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt."
-      //                                       className="h-full w-full object-cover object-center"
-      //                                     />
-      //                                   </div>
-
-      //                                   <div className="ml-4 flex flex-1 flex-col">
-      //                                     <div>
-      //                                       <div className="flex justify-between text-base font-medium text-gray-900">
-      //                                         <h3>
-      //                                           <a href="#">{food.name}</a>
-      //                                         </h3>
-      //                                         <p className="ml-4">
-      //                                           ${food.price}
-      //                                         </p>
-      //                                       </div>
-      //                                       <p className="mt-1 text-sm text-gray-500">
-      //                                         {food.vendor}
-      //                                       </p>
-      //                                     </div>
-      //                                     <div className="flex flex-1 items-end justify-between text-sm">
-      //                                       <p className="text-gray-500">
-      //                                         Qty 1
-      //                                       </p>
-
-      //                                       <div className="flex">
-      //                                         <button
-      //                                           onClick={() =>
-      //                                             removeItemHandler(food)
-      //                                           }
-      //                                           type="button"
-      //                                           className="font-medium text-red-600 hover:text-red-500"
-      //                                         >
-      //                                           Remove
-      //                                         </button>
-      //                                       </div>
-      //                                     </div>
-      //                                   </div>
-      //                                 </li>
-      //                               ))}
-      //                             </ul>
-      //                           </div>
-      //                         </div>
-      //                       )}
-      //                     </div>
-      //                   </div>
-      //                 </div>
-
-      //                 <div class="border-t border-gray-200 py-6 px-4 sm:px-6">
-      //                   <div class="flex justify-between text-base font-medium text-gray-700">
-      //                     <p>Subtotal</p>
-      //                     <p>$22.00</p>
-      //                   </div>
-      //                   <div class="flex justify-between text-base font-medium text-gray-700">
-      //                     <p>Delivery fee</p>
-      //                     <p>$2.00</p>
-      //                   </div>
-
-      //                   <div class="flex justify-between text-base font-medium text-gray-900">
-      //                     <p>Total</p>
-      //                     <p>$262.00</p>
-      //                   </div>
-
-      //                   <div class="mt-6">
-      //                     <Link href="/Payment">
-      //                       <div class="flex items-center justify-center rounded-md border border-transparent bg-red-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-red-700">
-                            
-      //                         Place order
-      //                       </div>
-      //                     </Link>
-      //                   </div>
-      //                   <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
-      //                     <p>
-      //                       or
-      //                       <button
-      //                         type="button"
-      //                         class="p-1 font-medium text-red-600 hover:text-red-500"
-      //                       >
-      //                         Continue Shopping
-      //                         <span aria-hidden="true"> &rarr;</span>
-      //                       </button>
-      //                     </p>
-      //                   </div>
-      //                 </div>
-      //               </div>
-      //             </div>
-      //           </div>
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </div>
-      // </div>
 
 
 }
